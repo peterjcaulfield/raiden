b7f51d0de79571fb4f907af61a055f376f99c7e5
Peter Caulfield <peterjcaulfield@gmail.com>
2016-06-03 16:29:22 +0100

    add wiremock and basic wiring of a shell command using commander

--- grunt/tasks/options/exec.js
+++ grunt/tasks/options/exec.js
@@ -4,11 +4,5 @@ module.exports = {
   },
   wiremock_stop: {
     cmd: './node_modules/pm2/bin/pm2 flush && ./node_modules/pm2/bin/pm2 delete all'
-  },
-  webpack_server_start: {
-    cmd: 'webpack_dev_server'
-  },
-  webpack_server_stop: {
-    cmd: ''
   }
 }
